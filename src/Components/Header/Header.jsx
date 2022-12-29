155a217e5829ef0939804768e5b6d1ec44508d34
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-12-29 22:47:35 +0530

    clint site error fixed

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import {
+  Home,
+  HomeOutlined,
+  Add,
+  AddOutlined,
+  SearchOutlined,
+  Search,
+  AccountCircle,
+  AccountCircleOutlined,
+} from "@mui/icons-material";
+import MovieIcon from "@mui/icons-material/Movie";
+
+const Header = () => {
+  const [tab, setTab] = useState(window.location.pathname);
+  return (
+    <div className="header">
+      <Link to="/" onClick={() => setTab("/")}>
+        {tab === "/" ? <Home style={{ color: "black" }} /> : <HomeOutlined />}
+      </Link>
+
+      <Link to="/media" onClick={() => setTab("/media")}>
+        {tab === "/media" ? <Add style={{ color: "black" }} /> : <MovieIcon />}
+      </Link>
+
+      <Link to="/search" onClick={() => setTab("/search")}>
+        {tab === "/search" ? (
+          <Search style={{ color: "black" }} />
+        ) : (
+          <SearchOutlined />
+        )}
+      </Link>
+
+      <Link to="/account" onClick={() => setTab("/account")}>
+        {tab === "/account" ? (
+          <AccountCircle style={{ color: "black" }} />
+        ) : (
+          <AccountCircleOutlined />
+        )}
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
